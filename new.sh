aed8cfbdaa43b34ebc1f6d1c2ba1690cf8edcaca
fengkx <liangkx8237@gmail.com>
2020-10-28 09:31:47 +0800

    fix: DIRNAME like __diranme in nodejs

--- new.sh
+++ new.sh
@@ -3,13 +3,15 @@ then
 	echo 'ERROR: need paramter `./new.sh <problem name>`'
 	exit 1 
 fi
-path="$PWD/problems/$1"
+DIRNAME=$(dirname $0)
+path="$DIRNAME/problems/$1"
+echo $path
 if [ -d $path ]
 then
 	echo "ERROR: directory $path has existed"
 	exit 1
 fi
-cp -r "$PWD/problem_template" $path
+cp -r "$DIRNAME/problem_template" $path
 mv "$path/TTT.cpp" "$path/$1.cpp"
 sed -i "s/TTT/$1/g" "$path/$1.cpp"
 sed -i "s/TTT/$1/g" "$path/CMakeLists.txt"
